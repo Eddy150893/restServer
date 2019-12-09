e55b3bc6843c8004f6339354590e64ae22a1c3d9
Eddy150893 <eddypaz150893@gmail.com>
2019-12-09 16:41:35 -0600

    agregando archivo de config

--- server/server.js
+++ server/server.js
@@ -1,26 +1,37 @@
+require('./config/config');
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ extended: false }))
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', function (req, res) {
     res.json('get Usuario');
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', function (req, res) {
     let body = req.body
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
     console.log(req.body);
     res.json({ body });
 })
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', function (req, res) {
     let id = req.params.id;
     res.json({ id });
 });
 
-app.delete('/usuario', function(req, res) {
+app.delete('/usuario', function (req, res) {
     res.json('delete Usuario');
 })
-app.listen(3000, () => {
-    console.log('Escuchando puerto: ', 3000)
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT)
 });
