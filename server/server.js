edba048d4f8f91f3ca0e2ad02e7b22b56fcfc370
eddypaz <eddypaz150893@gmail.com>
2020-05-16 14:31:19 -0600

    modificando config

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ extended: false }));
 app.use(require("./routes/usuario"));
-mongoose.connect("mongodb://localhost:27017/cafe", 
+//"mongodb://localhost:27017/cafe"
+mongoose.connect(process.env.URLDB, 
                 {useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true},
                   (err, res) => {
   if (err) throw err;
