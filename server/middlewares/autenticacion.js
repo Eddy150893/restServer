fc0e1c8de5ed18dc21a39ea535fbaf2bdd01c805
eddypaz <eddypaz150893@gmail.com>
2020-05-30 17:43:25 -0600

    Login normal seccion 10

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -4,6 +4,7 @@
 const jwt=require('jsonwebtoken');
 let verificaToken=(req,res,next)=>{
     let token=req.get('token');//asi se obtiene los headers
+    console.log(token);
     jwt.verify(token,process.env.SEED,(err,decoded)=>{
         if(err){
             return res.status(401).json({
